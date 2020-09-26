8f807dd95c4ec217dd9bf7ab228f34e053ffbb87
Negatives-One <49993384+Negatives-One@users.noreply.github.com>
2020-09-26 00:11:28 -0300

    :sparkles: Player Physics Movement

--- Assets/Scripts/PlayerController.gd
+++ Assets/Scripts/PlayerController.gd
@@ -1,14 +1,28 @@
 extends KinematicBody2D
 
-export(float) var speed : float = 200
+class_name Player
+
+export(float) var Acelleration : float = 20
+export(float) var MaxSpeed : float = 200
 var _velocity : Vector2 = Vector2(0, 0)
-var _aceleration : Vector2 = Vector2(0, 0)
+var _acelleration : Vector2 = Vector2(0, 0)
+
+var _target : Vector2
+var _moving : bool = false
 
 func _ready() -> void:
 	pass
 
+func _unhandled_input(event):
+	if(event == Input.is_action_just_pressed("LClick")):
+		_moving = true
+	elif(event == Input.is_action_just_released("LClick")):
+		_moving = false
+
 func _physics_process(delta) -> void:
+	_target = get_global_mouse_position()
+	_velocity = move_and_slide(_velocity, Vector2(0, -1), false, 4, 0.78598, false)
 	pass
 
 func AddForce(direction : Vector2, magnitude : float) -> void:
-	_aceleration += direction.normalized() * magnitude
+	_acelleration += direction.normalized() * magnitude
