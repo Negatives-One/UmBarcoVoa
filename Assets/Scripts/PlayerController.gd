e88b18948c7ee01b8a4b3b8da2f65b6d9a5bd26b
Negatives-One <49993384+Negatives-One@users.noreply.github.com>
2020-09-27 13:25:21 -0300

    :recycle: Moviment Refactor

--- Assets/Scripts/PlayerController.gd
+++ Assets/Scripts/PlayerController.gd
@@ -8,21 +8,43 @@ var _velocity : Vector2 = Vector2(0, 0)
 var _acelleration : Vector2 = Vector2(0, 0)
 
 var _target : Vector2
-var _moving : bool = false
+enum States {Acelerando, Estavel, Desacelerando, Parado}
+var currentState : int
 
 func _ready() -> void:
+	currentState = States.Parado
 	pass
 
 func _unhandled_input(event):
-	if(event == Input.is_action_just_pressed("LClick")):
-		_moving = true
-	elif(event == Input.is_action_just_released("LClick")):
-		_moving = false
+	if event is InputEventMouseButton:
+		if event.pressed and event.button_index == BUTTON_LEFT:
+			currentState = States.Acelerando
+		elif event.is_action_released("LClick"):
+			currentState = States.Desacelerando
+		else:
+			pass
 
 func _physics_process(delta) -> void:
+	print(_velocity, "/n", _acelleration)
 	_target = get_global_mouse_position()
+	
+	if(_velocity.length() >= MaxSpeed):
+		currentState = States.Estavel
+	
+	FSM()
+	_velocity += _acelleration
+	_velocity.clamped(MaxSpeed)
 	_velocity = move_and_slide(_velocity, Vector2(0, -1), false, 4, 0.78598, false)
-	pass
+
+func FSM() -> void:
+	if(currentState == States.Acelerando):
+		AddForce(_target - global_position, Acelleration)
+	elif(currentState == States.Estavel):
+		pass
+	elif(currentState == States.Desacelerando):
+		_velocity = _velocity.linear_interpolate(Vector2.ZERO, Acelleration)
+	else:
+		pass
 
 func AddForce(direction : Vector2, magnitude : float) -> void:
 	_acelleration += direction.normalized() * magnitude
