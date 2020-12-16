3b4b880edf1ea70fa8e00418f5d8197356ac0f59
Bruno Carvalho Lima <brunoorvalho@hotmail.com>
2020-12-16 16:14:29 -0300

    pre wind current

--- Assets/Scripts/Current.gd
+++ Assets/Scripts/Current.gd
@@ -18,14 +18,24 @@ var initialXPosition : float
 
 export(float) var speed : float = 1000
 
+var isStrong : bool = false
+
 func _ready() -> void:
+	gravity_vec = Vector2(direction, 0)
+	if isStrong:
+		gravity = get_parent().magnitudeStrong
+		$pixel.self_modulate = Color(1, 0, 0, 0.8)
+	else:
+		gravity = get_parent().magnitudeWeak
+		$pixel.self_modulate = Color(1, 0, 0, 0.4)
 	$CollisionShape2D.shape.extents = Vector2(get_viewport_rect().size.x/2, 270/2)
 	$CollisionShape2D.position.x = get_viewport_rect().size.x/2
 	$pixel.scale.x = get_viewport_rect().size.x #d(Vector2(get_viewport_rect().size.x, 270))
-	pass
+	$VisibilityNotifier2D.rect = Rect2(0, 0, get_viewport_rect().size.x, -270)
 
 func _physics_process(delta: float) -> void:
-	LinearXInterpolation(targetXPosition, speed, delta)
+	position.x += direction * (speed + get_parent().target.get_parent().linear_velocity.x) * delta
+	#LinearXInterpolation(targetXPosition, speed, delta)
 
 func LinearXInterpolation(targetPos : float, velocity : float, physicsDelta : float) -> void:
 	currentState = states.Moving
@@ -33,3 +43,11 @@ func LinearXInterpolation(targetPos : float, velocity : float, physicsDelta : fl
 	if initialXPosition > targetPos:
 		position.x = targetPos
 		currentState = states.Idle
+
+
+func _on_VisibilityNotifier2D_screen_exited() -> void:
+	var value : int = currentSpace * (get_viewport_rect().size.y/4)
+	if !get_parent().openSpaces.has(value):
+		get_parent().openSpaces.insert(currentSpace, value)
+		get_parent().activeWindsCurrents -= 1
+		call_deferred("queue_free")
--- Assets/Scripts/WindCurrents.gd
+++ Assets/Scripts/WindCurrents.gd
@@ -5,6 +5,9 @@ class_name WindCurrents
 export(NodePath) var targetPath : NodePath
 var target : Node
 
+export(NodePath) var animationWarningPath : NodePath
+var animationPlayer : AnimationPlayer
+
 var current : PackedScene = preload("res://Assets/Scenes/Current.tscn")
 
 onready var screenSize : Vector2 = get_viewport_rect().size
@@ -24,17 +27,26 @@ var timer : Timer = Timer.new()
 
 var openSpaces : Array = []
 
+var alertRotation : int = 0
+
+#instantiating vars
+var dirCurrent : Array = [0, 0, 0]
+var selectedSpace : Array = [0, 0, 0]
+
 func _ready() -> void:
+	for i in $Warnings.get_children():
+		i.self_modulate = Color(1, 0, 0, 0)
 	screenDivisionValue = screenSize.y / screenDivisions
 	for i in range(screenDivisions):
 		openSpaces.append((i) * screenDivisionValue)
 	target = get_node(targetPath)
+	animationPlayer = get_node(animationWarningPath)
 	var _error : int = timer.connect("timeout", self, "_on_timer_timeout") 
 	add_child(timer)
 	timer.autostart = false
 	timer.wait_time = timeBetweenSpawns
 
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	global_position.x = target.global_position.x - screenSize.x/2
 
 func Enable() -> void:
@@ -46,20 +58,42 @@ func Disable() -> void:
 	timer.stop()
 
 func _on_timer_timeout():
-	#for i in range(2):
-	if activeWindsCurrents < screenDivisions:
+	if activeWindsCurrents < 3:
+		#PlayWarning()
 		CreateCurrent()
 
 func CreateCurrent() -> void:
-	var newCurrent : Current = current.instance()
-	var possibleDirections : Array = [1, -1]
-	newCurrent.direction = possibleDirections[randi() % possibleDirections.size()]
-	call_deferred('add_child', newCurrent)
-	var selectedSpace : int = randi() % openSpaces.size()
-	if newCurrent.direction == 1:
-		newCurrent.position = Vector2(-screenSize.x, -openSpaces[selectedSpace])
+	randomize()
+	var warnings : Array = $Warnings.get_children()
+	dirCurrent.append(pow(-1, randi() % 2))
+	selectedSpace.append(randi() % openSpaces.size())
+	print(warnings[activeWindsCurrents].name)
+	if dirCurrent[activeWindsCurrents] == 1: 
+		warnings[activeWindsCurrents].position = Vector2(screenSize.x * 0.05, -openSpaces[selectedSpace[activeWindsCurrents]] - screenDivisionValue/2)
 	else:
-		newCurrent.position = Vector2(screenSize.x, -openSpaces[selectedSpace])
-	openSpaces.remove(selectedSpace)
-	newCurrent.currentSpace = selectedSpace
+		warnings[activeWindsCurrents].position = Vector2(screenSize.x * 0.95, -openSpaces[selectedSpace[activeWindsCurrents]] - screenDivisionValue/2)
+	animationPlayer.play("alert" + str(activeWindsCurrents))
 	activeWindsCurrents += 1
+
+#func PlayWarning() -> void:
+#	var direction : int = pow(-1, randi() % 2)
+#	var selectedSpace : int = randi() % openSpaces.size()
+#	if direction == 1:
+#		newCurrent.global_position = Vector2(target.global_position.x - screenSize.x*1.5, -openSpaces[selectedSpace])
+#	else:
+#		newCurrent.position = Vector2(target.global_position.x + screenSize.x/2, -openSpaces[selectedSpace])
+
+
+func _on_WarningsPlayer_animation_finished(_anim_name: String) -> void:
+	var newCurrent : Current = current.instance()
+	newCurrent.direction = dirCurrent.pop_front()
+	newCurrent.currentSpace = selectedSpace.pop_front()
+	newCurrent.isStrong = true
+	if randi() % 2:
+		newCurrent.isStrong = false
+	call_deferred('add_child', newCurrent)
+	if newCurrent.direction == 1:
+		newCurrent.global_position = Vector2(position.x - screenSize.x, -openSpaces[newCurrent.currentSpace])
+	else:
+		newCurrent.global_position = Vector2(position.x + screenSize.x, -openSpaces[newCurrent.currentSpace])
+	openSpaces.remove(newCurrent.currentSpace)
